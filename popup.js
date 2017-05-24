7e7dcfe4775fad7beb95fdf30b0cd2ccbbf808a1
Andres Torres <andres.torres@speeracademy.org>
2017-05-24 15:23:48 -0500

    whole new extensions

---
+++ popup.js
@@ -0,0 +1,8 @@
+function loadUrls() {
+// fetch urls from textarea and load them
+}
+function saveUrls() {
+    //Fetch urls and save it in chrome storage
+}
+document.getElementByid('button').addEventListener('click',loadUrls);
+document.getElementByid('button').addEventListener('click',saveUrls);
